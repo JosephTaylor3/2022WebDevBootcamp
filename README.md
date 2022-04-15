1d395de4fadedf3b85b5b594d630768a11e4743f
jmcmanus333 <joseph.mcmanus.333@gmail.com>
2022-04-15 15:47:49 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 2022_Web_Development_Bootcamp
+All notes, files, work and projects for the 2022 Full-Stack Web Development Bootcamp 
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // add new file paths into content for any files that use Tailwind CSS 
+  // see https://tailwindcss.com/docs/content-configuration
+  content: ["./1.BootcampLessonNotes/Week1Notes.html",
+            "./1.BootcampLessonNotes/Week2Notes.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
